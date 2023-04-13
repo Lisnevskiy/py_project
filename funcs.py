2a0f65ea0d5f84e2ec0cfb9deb8c153724c22a1b
Lisnevskiy <Lisnevskiy14@gmail.com>
2023-04-13 20:50:20 +0300

    added function

--- funcs.py
+++ funcs.py
@@ -1,2 +1,6 @@
 def get_sum_numbers(first_number: int, second_number: int):
     return first_number + second_number
+
+
+def get_difference_numbers(first_number: int, second_number: int):
+    return first_number - second_number
